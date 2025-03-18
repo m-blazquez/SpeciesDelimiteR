eb1f2c953497db5a3b2288dc49961cfd930317a5
m-blazquez <m.blazquezva@gmail.com>
2025-03-18 22:18:00 +0100

    Changes to the run_GMYC function

--- R/parse_ASAP.R
+++ R/parse_ASAP.R
@@ -1,10 +1,17 @@
 
-parse_ASAP <- function(ASAP_output, method = NULL) {
+parse_ASAP <- function(ASAP_output, prepare_ASAP_input = NULL) {
 
-  # Make sure a valid method is specified
-  if (is.null(method) || !method %in% c("JC69", "K80", "p_distances")) {
+  # Make sure a valid value for prepare_ASAP_input is specified
+  if (is.null(prepare_ASAP_input) || !prepare_ASAP_input %in% c("JC69", "K80", "p_distances")) {
 
-    stop("ERROR: Invalid method. Please specify either 'JC69', 'K80' or 'p_distances'")
+    stop("ERROR: Invalid value for prepare_ASAP_input argument. Please specify either TRUE or FALSE")
+
+  }
+
+  # Return to original specimen names if the prepare_ASAP_input function was used
+  if (prepare_ASAP_input) {
+
+    ASAP_output$V1 <- substr(ASAP_output$V1, 5, nchar(ASAP_output$V1))
 
   }
 
--- R/run_GMYC.R
+++ R/run_GMYC.R
@@ -3,80 +3,68 @@ run_GMYC <- function(phylogeny, outgroup = NULL, method = NULL) {
 
   # Make sure that the tree is ultrametric. If not, return an error message.
   if(ape::is.ultrametric(phylogeny) == FALSE) {
-
     stop("ERROR: The tree is not ultrametric.")
-
   }
 
   # Ensure a valid method is specified
   if (is.null(method) || !method %in% c("single", "multiple")) {
-
     stop("ERROR: You must specify a valid method ('single' or 'multiple').")
-
   }
-
 
   # Remove outgroup sequences (if provided)
   if (!is.null(outgroup) && length(outgroup) > 0) {
-
     for (i in seq_along(outgroup)) {
-
       if (outgroup[i] %in% phylogeny$tip.label) {
-
         phylogeny <- drop.tip(phylogeny, outgroup[i])
-
       } else {
-
         stop(paste("ERROR: Outgroup", outgroup[i], "not found in the phylogeny."))
-
       }
     }
   } else {
-
     message("No outgroup provided or outgroup vector is empty. Proceeding with the full tree.")
-
   }
 
-  # Runs GMYC using the splits package, using the single- or multiple- threshold options.
-  if(method == "single") {
-
+  # Runs GMYC using the splits package, using the single- or multiple-threshold options.
+  if (method == "single") {
     GMYC <- splits::gmyc(phylogeny, method = "single")
     GMYC <- spec.list(GMYC)
-    return(GMYC)
 
+    # Ensure the directory exists before writing the file
+    if (!dir.exists("GMYC/Single")) {
+      dir.create("GMYC/Single", recursive = TRUE)
+    }
+
+    # Write the file
+    write.table(GMYC,
+                file = "GMYC/Single/GMYCs_output.txt",
+                sep = "\t",
+                row.names = FALSE)
+
+    # Tell the user where the new file is
+    message(paste0("The file 'GMYCs_output.txt' has been created at '", getwd(), "/GMYC/Single'"))
+
+    return(GMYC)  # Return after writing file
   }
 
-  if(method == "multiple") {
-
+  if (method == "multiple") {
     GMYC <- splits::gmyc(phylogeny, method = "multiple")
     GMYC <- spec.list(GMYC)
-    return(GMYC)
 
-  }
+    # Ensure the directory exists before writing the file
+    if (!dir.exists("GMYC/Multiple")) {
+      dir.create("GMYC/Multiple", recursive = TRUE)
+    }
 
-  # Save the putative species file
-  if(method == "single") {
-
+    # Write the file
     write.table(GMYC,
-                file = "GMYCs_output.txt",
+                file = "GMYC/Multiple/GMYCm_output.txt",
                 sep = "\t",
                 row.names = FALSE)
 
     # Tell the user where the new file is
-    message(paste0("The file 'GMYCs_output.txt' has been created at '", getwd(), "'"))
+    message(paste0("The file 'GMYCm_output.txt' has been created at '", getwd(), "/GMYC/Multiple'"))
 
+    return(GMYC)  # Return after writing file
   }
-
-  if(method == "multiple") {
-
-    write.table(GMYC,
-                file = "GMYCm_output.txt",
-                sep = "\t",
-                row.names = FALSE)
-
-    # Tell the user where the new file is
-    message(paste0("The file 'GMYCm_output.txt' has been created at '", getwd(), "'"))
-
-  }
-
 }
+
