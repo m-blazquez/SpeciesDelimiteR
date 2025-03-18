bc15c027a0a679851defaecf52c9bbbaa221d2fb
m-blazquez <m.blazquezva@gmail.com>
2025-03-18 22:08:10 +0100

    Changes to the parse_ASAP function

--- R/parse_ASAP.R
+++ R/parse_ASAP.R
@@ -8,62 +8,19 @@ parse_ASAP <- function(ASAP_output, method = NULL) {
 
   }
 
-  # Parse the ASAP output based on the method used to produce it
-  if(method == "JC69") {
+  # Assign new column names
+  colnames(ASAP_output) <- c("Specimen", "ASAP_K80_species")
 
-    # Assign new column names
-    colnames(ASAP_output) <- c("Specimen", "ASAP_JC69_species")
+  # Write new file
+  write.table(ASAP_output,
+              file = "ASAP/ASAP_parsed_output.txt",
+              sep = "\t",
+              row.names = FALSE)
 
-    # Write new file
-    write.table(ASAP_output,
-                file = "ASAP_JC69_parsed_output.txt",
-                sep = "\t",
-                row.names = FALSE)
+  # Tell the user where the new file is
+  message(paste0("The file 'ASAP_parsed_output.txt' has been created at '", getwd(), "/ASAP'"))
 
-    # Tell the user where the new file is
-    message(paste0("The file 'ASAP_JC69_parsed_output.txt' has been created at '", getwd(), "'"))
-
-    # Return the dataframe
-    return(ASAP_output)
-
-  }
-
-  if(method == "K80") {
-
-    # Assign new column names
-    colnames(ASAP_output) <- c("Specimen", "ASAP_K80_species")
-
-    # Write new file
-    write.table(ASAP_output,
-                file = "ASAP_K80_parsed_output.txt",
-                sep = "\t",
-                row.names = FALSE)
-
-    # Tell the user where the new file is
-    message(paste0("The file 'ASAP_K80_parsed_output.txt' has been created at '", getwd(), "'"))
-
-    # Return the dataframe
-    return(ASAP_output)
-
-  }
-
-  if(method == "p_distances") {
-
-    # Assign new column names
-    colnames(ASAP_output) <- c("Specimen", "ASAP_p_distances_species")
-
-    # Write new file
-    write.table(ASAP_output,
-                file = "ASAP_p_distances_parsed_output.txt",
-                sep = "\t",
-                row.names = FALSE)
-
-    # Tell the user where the new file is
-    message(paste0("The file 'ASAP_p_distances_parsed_output.txt' has been created at '", getwd(), "'"))
-
-    # Return the dataframe
-    return(ASAP_output)
-
-  }
+  # Return the dataframe
+  return(ASAP_output)
 
 }
